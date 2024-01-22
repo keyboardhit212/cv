4f2b75eed1f4d548358f0fc53aa3f24aa1c57485
keyboardhit212 <rjudd.celis@gmail.com>
2024-01-22 14:25:01 +0800

    Added Links

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -3,9 +3,9 @@ import './Contact.css';
 export default function Contact() {
     return (
         <section className='contact'>
-            <a href=""><img src="images/facebook-svgrepo-com.svg" width="25px" alt="Facebook" title="Facebook"/></a>
+            <a href="https://www.facebook.com/rjudd.celis"><img src="images/facebook-svgrepo-com.svg" width="25px" alt="Facebook" title="Facebook"/></a>
             <a href=""><img src="images/linkedin-svgrepo-com.svg" width="25px" alt="LinkedIn" title="LinkedIn"/></a>
-            <a href=""><img src="images/github-svgrepo-com.svg" width="25px" alt="GitHub" title="GitHub"/></a>
+            <a href="https://github.com/keyboardhit212"><img src="images/github-svgrepo-com.svg" width="25px" alt="GitHub" title="GitHub"/></a>
         </section>
     );
 }
