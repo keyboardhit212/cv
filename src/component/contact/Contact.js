5fa281ca499955aa33340bfd1e0fd10584db0d6b
keyboardhit212 <rjudd.celis@gmail.com>
2024-02-02 17:16:59 +0800

    Added LinkedIn

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <section className='contact'>
             <a href="https://www.facebook.com/rjudd.celis"><img src="images/facebook-svgrepo-com.svg" width="25px" alt="Facebook" title="Facebook"/></a>
-            <a href=""><img src="images/linkedin-svgrepo-com.svg" width="25px" alt="LinkedIn" title="LinkedIn"/></a>
+            <a href="https://ph.linkedin.com/in/r-judd-celis-8128872a7"><img src="images/linkedin-svgrepo-com.svg" width="25px" alt="LinkedIn" title="LinkedIn"/></a>
             <a href="https://github.com/keyboardhit212"><img src="images/github-svgrepo-com.svg" width="25px" alt="GitHub" title="GitHub"/></a>
         </section>
     );
